5dd6380ae148172585d1615742b85bca58abe66e
gwlee <gwlee@goodlab.kr>
2017-11-02 11:37:02 +0900

    소스트리 한글깨짐 현상 해결
    메모장으로 할경우 소스트리 한글깨짐현상생김
    비주얼코드로 작성

--- application/views/Login.php
+++ application/views/Login.php
@@ -1,20 +1,21 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <div> 
     <div>
-    <h2>�α���</h2>
-    <a href="http://www.good8288.com" type="button">���ر� �̵�</a>
+    <h2>로그인</h2>
+    <a href="http://www.good8288.com" type="button">굳앤굳 이동</a>
 
     </div>   
 
     <form action="a" method="post">
     <table border="1">
     <tr>
-    <td><label class="control-label" for="u_id">���̵�</label></td>
+    <td><label class="control-label" for="u_id">아이디</label></td>
     <td><input type="text" id="u_id" name="u_id" placeholder="id"></td>
-    <td rowspan="2"><input type="submit" class="btn btn-primary" value="�α���" /></td>
+    <td rowspan="2"><input type="submit" class="btn btn-primary" value="로그인" /></td>
     </tr>
     <tr>
-    <td><label class="control-label" for="password">��й�ȣ</label></td>
-    <td><input type="password" id="password" name="password" placeholder="��й�ȣ"></td>
+    <td><label class="control-label" for="password">비밀번호</label></td>
+    <td><input type="password" id="password" name="password" placeholder="비밀번호"></td>
     </tr>
     
     </table>

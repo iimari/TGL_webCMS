93ce6f17c711bb1a0f183d0816bd9c415ad2e4eb
leeminwoo <iimari@goodlab.kr>
2017-11-03 16:13:36 +0900

    [ 로그인 섹션 검증 구현  ]
       - 로그인 세션 확인
       - 세션이 없을 경우 메인으로 돌아감
       - Dashboard에 로그인한 유저 확인을 위한 echo $u_id

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -4,19 +4,29 @@ class Main extends CI_Controller{
     function __construct()
     {
         parent::__construct();
+        $this->load->helper('url');
     }
+
 
     public function index()
     {
-       $this->load->view('Dashboard');       
+        if($this->session->userdata('logged_in')){
+            $session_data = $this->session->userdata('logged_in');
+            $data['u_id'] = $session_data['u_id'];
+            $this->load->view('Dashboard', $data);
+        }else {
+            redirect('/','refresh');
+        }
+       
     }
+
+
     public function _remap($method) {
         
         // 헤더 include
         $this->load->view('Head'); 
         // 사이드 메뉴 include
         $this->load->view('Menu'); 
-       
         
         //바디
         if(method_exists($this, $method)) {
@@ -26,7 +36,6 @@ class Main extends CI_Controller{
         // 푸터 include
         $this->load->view('Footer');
     }
-
 }
 
 ?>
--- application/views/Dashboard.php
+++ application/views/Dashboard.php
@@ -2,6 +2,7 @@
 <div> 
     <div>
         <h2>대쉬보드입니다.</h2>
+        <?php echo $u_id ?>
     </div>
 </div>
 

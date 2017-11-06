9c7b42d4bbde7f698d4ae35816c642d10231d721
leeminwoo <iimari@goodlab.kr>
2017-11-06 16:33:40 +0900

    [ 도메인, 호스팅, 유지보수 상태 확인 구현 ]
       - 도메인 호스티 유지보수 상태를 확인할 수 있는 기능 구현 완료

--- application/views/Site_info.php
+++ application/views/Site_info.php
@@ -1,61 +1,67 @@
+<?php 
+   $h_startdate = 1;
+   $d_startdate = 1;
+   $m_startdate = null;
+?>
 <div> 
     <div>
         <h2>사이트관리 입니다.</h2>
         <!-- <?php echo $u_id ?> -->
-        <a href="<?php echo site_url('/Main/site_info'); ?>">생성</a>
-        <a href="<?php echo site_url('/Main/site_info'); ?>">엑셀다운</a>
+            <a href="<?php echo site_url('/Main/site_info'); ?>">생성</a>
+            <a href="<?php echo site_url('/Main/site_info'); ?>">엑셀다운</a>
+            </br>            
+            </br>    
         <div>
-        </br>
-        <?php 
-        foreach($list as $lt)
-        {
-            $h_startdate = 0;
-            $d_startdate = 0;
-            $m_startdate = null;
-
-            if($h_startdate == null)
-            {
-                echo '호스팅/비활성화';                
-                echo "\t";
-            }else {
-                echo '호스팅';                
-                echo "\t";
-            }
-            if($d_startdate == null)
-            {
-                echo '도메인/비활성화';
-                echo "\t";
-            }else {
-                echo '도메인';                
-                echo "\t";
-            }
-            if($m_startdate == null)
-            {
-                echo '유지보수/비활성화';
-                echo "\t";
-            }else {
-                echo '유지보수';                
-                echo "\t";
-            }
-            ?>
-            
+            <?php foreach($list as $lt){         
+                    if($lt['c_hosting_check'] == '0'){
+                        ?>
+                        <div>비활성화/호스팅</div>
+                        <?php
+                        echo "\t";
+                    }else {
+                        ?>
+                        <div>활성화/호스팅</div>
+                        <?php
+                        echo "\t";
+                    }
+                    if($lt['c_domain_check'] == '0'){
+                        ?>
+                        <div>비활성화/도메인</div>
+                        <?php     
+                        echo "\t";                   
+                    }else {
+                        ?>
+                        <div>활성화/도메인</div>
+                        <?php                      
+                        echo "\t";  
+                    }
+                    if($lt['c_managerment_check'] == '0'){
+                        ?>
+                        <div>비활성화/유지보수</div>
+                        <?php                        
+                        echo "\t";
+                    }else {
+                        ?>
+                        <div>활성화/유지보수</div>                                     
+                        <?php                        
+                    } 
+                ?>            
             <div>
-            <?php
-            echo $lt['c_name'];
-            echo "\t";
-            echo $lt['c_homepage'];            
-            echo $lt['c_manager'];
-            echo "\t";
-            echo $lt['c_phone'];            
-            ?>
-            </br>
-            </br>
-            <?php
-        }
-                
-            ?>
-            </div>        
-        <div>
+                <?php
+                    echo $lt['c_name'];
+                    echo "\t";
+                    echo $lt['c_homepage'];            
+                    echo "\t";
+                    echo $lt['c_manager'];
+                    echo "\t";
+                    echo $lt['c_phone'];            
+                ?>
+                </br>
+                </br>
+            </div>
+
+            <?php } ?>
+        </div>                
     </div>
 </div>
 

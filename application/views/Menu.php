0d07b37e603281fec77bdcd049c0ab972d3af9d7
leeminwoo <iimari@goodlab.kr>
2017-11-06 11:40:09 +0900

    [ 사이트 관리 페이지로 전체관리목록을 보기위한 메뉴 구성 ]
         - ul li 구성으로 메뉴트리 작성

--- application/views/Menu.php
+++ application/views/Menu.php
@@ -2,3 +2,12 @@
     <a href="<?php echo site_url('/login/sign_in') ?>">Logout</a>
 </div>
 <?php echo '메뉴 입니다.' ?>
+<ul>
+    <li>대시보드</li>
+    <li>사이트관리</li>
+    <ul>
+        <li>유지보수</li>
+        <li>호스팅</li>
+        <li>도메인</li>
+    </ul>
+</ul>

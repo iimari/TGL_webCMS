4aba2d463920034cfd41e2f1b3653dfaf85c4817
gwlee <gwlee@goodlab.kr>
2017-11-07 17:29:29 +0900

    [ 업체정보, 호스팅,도메인 수정완료]
       # 사이트 관리 페이지에서 업체정보 클릭시 업체정보, 호스팅, 도메인 정보 가져옴
       # 업체정보, 호스팅, 도메인 정보 수정가능
       # 팝업 닫기 버튼 추가
       # 수정취소 버튼 추가

--- application/models/Manager_model.php
+++ application/models/Manager_model.php
@@ -15,10 +15,18 @@ class Manager_model extends CI_Model {
     //업체상세정보, 수정시 불러오기
     function get_company_detailinfo($c_num)
     {              
-        return $this->db->get_where('company', array('c_num'=>$c_num))->row();        
+       return $this->db->get_where('company', array('c_num'=>$c_num))->row();        
     }
-    //수정 디비
-    public function update_content($c_num,$c_name,$c_manager,$c_phone,
+    function get_domain_detailinfo($d_num)
+    {              
+       return $this->db->get_where('domain_info', array('d_num'=>$d_num))->row();        
+    }
+    function get_hosting_detailinfo($h_num)
+    {              
+       return $this->db->get_where('hosting_info', array('h_num'=>$h_num))->row();        
+    }
+    //수정 디비 업체
+    public function c_update_content($c_num,$c_name,$c_manager,$c_phone,
                                     $c_mail,$c_fax,$c_homepage,$c_bigo1,$c_createdate){        
         
                  $data = array(         
@@ -37,6 +45,40 @@ class Manager_model extends CI_Model {
                              
         return $this->db->get_where('company', array('c_num'=>$c_num))->row();        
     }
+    //수정 디비 호스팅
+    public function h_update_content($h_num,$h_name,$h_startdate,$h_enddate,
+                                    $h_ftpid,$h_ftppw,$h_dbid,$h_dbpw){        
+
+                $data = array(         
+                'h_name'=>$h_name,
+                'h_startdate'=>$h_startdate,
+                'h_enddate'=>$h_enddate,
+                'h_ftpid'=>$h_ftpid,
+                'h_ftppw'=>$h_ftppw,
+                'h_dbid'=>$h_dbid,
+                'h_dbpw'=>$h_dbpw               
+                );
+                $this->db->where('h_num',$h_num);
+                $this->db->update('hosting_info',$data);
+
+
+            return $this->db->get_where('hosting_info', array('h_num'=>$h_num))->row();        
+    }
+    //수정 디비 도메인
+                public function d_update_content($d_num,$d_servicename,$d_id,$d_pw,$d_enddate){        
+
+                $data = array(         
+                'd_servicename'=>$d_servicename,
+                'd_id'=>$d_id,
+                'd_pw'=>$d_pw,
+                'd_enddate'=>$d_enddate               
+                );
+                $this->db->where('d_num',$d_num);
+                $this->db->update('domain_info',$data);
+
+
+            return $this->db->get_where('domain_info', array('d_num'=>$d_num))->row();        
+        }
 
     //삽입
     function insert_company_info($company_array,$hosting_array,$domain_array,$managerment_array)
--- application/views/Company_info.php
+++ application/views/Company_info.php
@@ -4,46 +4,84 @@
 
 <tr>
 <th><label class="control-label">업체명</label></th>
-<td><?=$data->c_name?></td>
+<td><?=$c_data->c_name?></td>
 </tr>
 <tr>
 <th><label class="control-label">담당자</label></th>
-<td><?=$data->c_manager?></td>
+<td><?=$c_data->c_manager?></td>
 </tr>
 <tr>
 <th><label class="control-label">휴대폰</label></th>
-<td><?=$data->c_phone?></td>
+<td><?=$c_data->c_phone?></td>
 </tr>
 <tr>
 <th><label class="control-label">메일</label></th>
-<td><?=$data->c_mail?></td>
+<td><?=$c_data->c_mail?></td>
 </tr>
 <tr>
 <th><label class="control-label">펙스</label></th>
-<td><?=$data->c_fax?></td>
+<td><?=$c_data->c_fax?></td>
 </tr>
 <tr>
 <th><label class="control-label">홈페이지</label></th>
-<td><?=$data->c_homepage?></td>
+<td><?=$c_data->c_homepage?></td>
 </tr>
 <tr>
 <th><label class="control-label">비고</label></th>
-<td><?=$data->c_bigo1?></td>
+<td><?=$c_data->c_bigo1?></td>
 </tr>
 <tr>
-<th><label class="control-label" >업체등록일</label></th>
-<td><?=$data->c_createdate?></td>
+<th><label class="control-label" >호스팅업체</label></th>
+<td><?=$h_data->h_name?></td>
+</tr>
+<tr>
+<th><label class="control-label" >호스팅시작일</label></th>
+<td><?=$h_data->h_startdate?></td>
+</tr>
+<tr>
+<th><label class="control-label" >호스팅종료일</label></th>
+<td><?=$h_data->h_enddate?></td>
+</tr>
+<tr>
+<th><label class="control-label" >FTP ID</label></th>
+<td><?=$h_data->h_ftpid?></td>
+</tr>
+<tr>
+<th><label class="control-label" >FTP PW</label></th>
+<td><?=$h_data->h_ftppw?></td>
+</tr>
+<tr>
+<th><label class="control-label" >DB ID</label></th>
+<td><?=$h_data->h_dbid?></td>
+</tr>
+<tr>
+<th><label class="control-label" >DB PW</label></th>
+<td><?=$h_data->h_dbpw?></td>
+</tr>
+<tr>
+<th><label class="control-label" >도메인서비스주소</label></th>
+<td><?=$d_data->d_servicename?></td>
+</tr>
+<th><label class="control-label" >도메인 ID</label></th>
+<td><?=$d_data->d_id?></td>
+</tr>
+<th><label class="control-label" >도메인 PW</label></th>
+<td><?=$d_data->d_pw?></td>
+</tr>
+<th><label class="control-label" >도메인관리만료일</label></th>
+<td><?=$d_data->d_enddate?></td>
 </tr>
 </table>
 </div>
 
 <form action="<?php echo site_url('/popup/detail_modify');?>" method="post">
 <div style="visibility:hidden">
-<input type="text" name="c_num" placeholder="업체번호" value = "<?=$data->c_num?>"></input><br>    
+<input type="text" name="c_num" placeholder="업체번호" value = "<?=$c_data->c_num?>"></input><br>    
 </div>
 <input class ="btn" type="submit" value="수정"/>
 </form>
 
+<a href="#" onclick="window.close(); return false;">닫기</a>
 
 <!-- <div>    
 <a href = "<?php// echo site_url('/popup/detail_modify');?>">수정</a>

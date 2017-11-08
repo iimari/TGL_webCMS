2f6548942635995f06fbad7d01e4fa013c9db316
leeminwoo <iimari@goodlab.kr>
2017-11-08 09:22:15 +0900

    [ 도메인, 유지보수, 호스팅 페이지 생성 ]
       # 사이트 하위 관리 메뉴를 보여줄 페이지를 생성

--- application/views/Head.php
+++
@@ -1,9 +0,0 @@
-<head>
-<meta http-equiv="Content-Type" content="text/html" charset="utf-8" />
-<meta http-equiv="X-UA-Comatible" content="IE=edge">
-<title>webCMS</title>
-<script src="//code.jquery.com/jquery.js"></script>
-</head>
-<?php
-echo '헤더입니다' 
-?>
